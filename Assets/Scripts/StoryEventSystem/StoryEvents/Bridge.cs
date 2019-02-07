26975673cec07df2be81cec84b35eb58bde82ace
Noah <noahpbrinkman@gmail.com>
2019-02-07 20:39:48 +0100

    Created order in events

---
+++ Assets/Scripts/StoryEventSystem/StoryEvents/Bridge.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    public void Remove(GameObject removeWhat)
+    {
+        removeWhat.SetActive(false);
+    }
+}
