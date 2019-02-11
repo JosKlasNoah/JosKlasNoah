7e67274959147bc8574113b3f2f1ca3b7ce2e05a
BroccoliTaart <j.hannes.b@hotmail.com>
2019-02-11 16:40:41 +0100

    Added magic (lightning) bolt        - has lightning trails        - has sparks        - lightning trails collide with world and spawn more sparks

---
+++ Assets/TestMoveBolt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMoveBolt : MonoBehaviour
+{
+    [SerializeField]
+    private float _range = 2;
+
+    [SerializeField]
+    private float _rate = 0.25f;
+
+    private Vector3 _startPosition;
+
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3( _startPosition.x + Mathf.Sin( Time.time / _rate) * _range, _startPosition.y, _startPosition.z );
+    }
+}
