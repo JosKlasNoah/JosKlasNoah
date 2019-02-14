c145d2af85b8ede4df5f67483d511d609262edc2
BroccoliTaart <j.hannes.b@hotmail.com>
2019-02-14 10:36:37 +0100

    Modified MovingObject script        - now uses velocity of the rigidbody correctly        - places Gizmos to indicate movement range

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class MovingObject : MonoBehaviour
 {
     [SerializeField, Tooltip("Distance in units the object travels on each axis in world space")]
@@ -14,17 +15,34 @@ public class MovingObject : MonoBehaviour
     private Vector3 _timeOffset;
 
     private Vector3 _startPosition;
+    private Vector3 _targetPosition;
     private Rigidbody _rb;
 
 
     private void Start()
     {
-        _startPosition = transform.position;
+        Debug.Assert( null != GetComponent<Rigidbody>(), "Object doesn't have a rigidbody" );
         _rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnValidate()
+    {
+        _startPosition = transform.position;
+        _targetPosition = transform.position + new Vector3( _speed.x * _range.x, _speed.y * _range.y, _speed.z * _range.z ) * .9f;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color( 0, 1, 0, 0.5f );
+        Gizmos.DrawSphere( _startPosition, 0.2f );
+        Gizmos.DrawSphere( _targetPosition, 0.2f );
     }
 
     void FixedUpdate()
     {
-        transform.position = new Vector3( _startPosition.x + Mathf.Sin( ( Time.time + _timeOffset.x ) * _speed.x ) * _range.x, _startPosition.y + Mathf.Sin( ( Time.time + _timeOffset.y ) * _speed.y ) * _range.y, _startPosition.z + Mathf.Sin( ( Time.time + _timeOffset.z ) * _speed.z ) * _range.z );
+        //transform.position = new Vector3( _startPosition.x + Mathf.Sin( ( Time.time + _timeOffset.x ) * _speed.x ) * _range.x, _startPosition.y + Mathf.Sin( ( Time.time + _timeOffset.y ) * _speed.y ) * _range.y, _startPosition.z + Mathf.Sin( ( Time.time + _timeOffset.z ) * _speed.z ) * _range.z );
+        
+        _rb.velocity = new Vector3( Mathf.Sin( ( Time.time + _timeOffset.x ) * _speed.x ) * _range.x, Mathf.Sin( ( Time.time + _timeOffset.y ) * _speed.y ) * _range.y, Mathf.Sin( ( Time.time + _timeOffset.z ) * _speed.z ) * _range.z );
+        Debug.Log( _rb.velocity );
     }
 }
