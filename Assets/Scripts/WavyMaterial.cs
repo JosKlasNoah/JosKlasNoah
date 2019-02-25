e978369f4d877e2a7699278c2810afc76e80fe4d
BroccoliTaart <j.hannes.b@hotmail.com>
2019-02-25 13:06:36 +0100

    reverted back to old render pipeline & altered materials reduced lights to better fit graphics

---
+++ Assets/Scripts/WavyMaterial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent( typeof( MeshFilter ) )]
+public class WavyMaterial : MonoBehaviour
+{
+    [SerializeField]
+    private float _amplitude;
+
+    [SerializeField]
+    private float _speed;
+
+    private MeshFilter _filter;
+
+    void Start()
+    {
+        _filter = GetComponent<MeshFilter>();
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3[] vertices = _filter.mesh.vertices;
+
+        for ( int i = 0; i < vertices.Length; i++ )
+        {
+            vertices[ i ] += Vector3.up * Mathf.Sin( ( Time.fixedTime ) * _speed ) * _speed * _amplitude;
+        }
+    }
+}
