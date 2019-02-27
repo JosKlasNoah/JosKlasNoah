ea68bffe695720a37e94cb408681b11302ef93f2
Noah Brinkman <noahpbrinkman@gmail.com>
2019-02-27 14:38:51 +0100

    added an elevator

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    public int up = -1;
+    private float t = 0;
+    private Vector3 startPos;
+    [SerializeField] Vector3 endPos = new Vector3(0,20, 0);
+    [SerializeField] private float speed = 1;
+
+    private void Start()
+    {
+        startPos = transform.localPosition;  
+    }
+
+    private void MoveElevator()
+    {
+        //transform.localPosition = new Vector3(transform.position.x, Mathf.Lerp(46.25f, 1, 0), transform.position.z);
+        transform.localPosition = Vector3.Lerp(startPos,startPos + endPos, t);
+    }
+
+    private void Update()
+    {
+        t += Time.deltaTime * up * speed;
+        Mathf.Clamp(t, 0, 1);
+       MoveElevator();
+    }
+
+}
