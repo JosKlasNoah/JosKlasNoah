f341066e890d03268d5cbef97acc2bf3367a0793
dimin69 <dimin69gaming@outlook.com>
2019-03-01 09:54:57 +0100

    no message

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Elevator : MonoBehaviour
 {
     private Vector3 startPos;
     [SerializeField] Vector3 endPos = new Vector3(0, 20, 0);
     [SerializeField] private float speed = 1;
     [SerializeField] private float waitTime = 5f;
+    [SerializeField] private AudioClip _audioClip;
+    [SerializeField] private AudioSource ac;
 
     /*private bool startTimer = false;
     private float timer = 5;*/
@@ -21,6 +24,8 @@ public class Elevator : MonoBehaviour
 
     public void StartPlatform()
     {
+        ac = GetComponent<AudioSource>();
+        ac.clip = _audioClip;
         if (!started)
         {
             StartCoroutine(MovePlatform());
@@ -33,6 +38,7 @@ public class Elevator : MonoBehaviour
     {
         while (true)
         {
+            ac.Play();
             while (transform.localPosition.y < startPos.y + endPos.y)
             {
                 transform.localPosition += Vector3.up * speed;
@@ -40,15 +46,16 @@ public class Elevator : MonoBehaviour
                 yield return new WaitForFixedUpdate();
             }
 
+            ac.Pause();
             yield return new WaitForSeconds(waitTime);
-
+            ac.Play();
             while (transform.localPosition.y > startPos.y)
             {
 
                 transform.localPosition -= Vector3.up * speed;
                 yield return new WaitForFixedUpdate();
             }
-
+            ac.Pause();
             yield return new WaitForSeconds(waitTime);
 
         }
